f500a49e64cd3b09c335e3522e1e23fe3ba2e4a2
michalwolski <wolak90@gmail.com>
2016-08-11 14:01:13 +0200

    fix

--- src/Get_tested.java
+++ src/Get_tested.java
@@ -4,8 +4,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import io.appium.java_client.ios.IOSDriver;
